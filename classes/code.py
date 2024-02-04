e5d5442f320fb6c64fe8b468cb8e462bcb8bffc4
Vadim <starinalokic@gmail.com>
2024-02-04 12:59:49 +0300

    Finished wor number 4

--- classes/code.py
+++ classes/code.py
@@ -19,15 +19,15 @@ class Student:
     def __str__(self):
         return f"""Имя: {self.name}
 Фамилия: {self.surname}
-Средняя оценка за домашнее задание: {self._avarage_grades()}
+Средняя оценка за домашнее задание: {self.avarage_grades()}
 Курсы в роцессе изучения: {', '.join(self.courses_in_progress) if len(self.courses_in_progress) != 0 else "Отсутствуют"}
 Завершенные курсы: {', '.join(self.finished_courses) if len(self.finished_courses) != 0 else "Отсутствуют"}"""
 
-    def _avarage_grades(self):
+    def avarage_grades(self):
         if len(self.grades) != 0:
             sum_grades = 0
             cout_grades = 0
-            for list_grades in self.grades.value():
+            for list_grades in self.grades.values():
                 for grade in list_grades:
                     sum_grades += grade
                     cout_grades += 1
@@ -36,7 +36,14 @@ class Student:
             return 0
 
     def __gt__(self, other):
-        return self._avarage_grades() > other._avarage_grades()
+        return self.avarage_grades() > other.avarage_grades()
+    
+    def __eq__(self, other):
+        return self.avarage_grades() == other.avarage_grades()
+
+    def __ne__(self, other):
+        return self.avarage_grades() != other.avarage_grades()
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -53,7 +60,7 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
-    def _avarage_grades(self):
+    def avarage_grades(self):
         if len(self.grades) != 0:
             sum_grades = 0
             cout_grades = 0
@@ -66,10 +73,17 @@ class Lecturer(Mentor):
             return 0
 
     def __str__(self):
-        return f'{super().__str__()}\nСредняя оценка за лекции: {self._avarage_grades()}'
+        return f'{super().__str__()}\nСредняя оценка за лекции: {self.avarage_grades()}'
 
     def __gt__(self, other):
-        return self._avarage_grades() > other._avarage_grades()
+        return self.avarage_grades() > other.avarage_grades()
+    
+    def __eq__(self, other):
+        return self.avarage_grades() == other.avarage_grades()
+
+    def __ne__(self, other):
+        return self.avarage_grades() != other.avarage_grades()
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -83,3 +97,65 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка данных'
+
+
+kirill = Student('Кирилл', 'Попов', 'муж')
+kirill.finished_courses += ['Основы языка програмирования Python']
+kirill.courses_in_progress += ['Git — система контроля версий']
+kirill.grades['Git — система контроля версий'] = [10, 9, 7, 10]
+
+
+oleg = Student('Олег', 'Фестралов', 'муж')
+oleg.finished_courses += ['Основы языка програмирования Python']
+oleg.courses_in_progress += ['ООП и работа с API']
+oleg.grades['ООП и работа с API'] = [9, 8, 10, 7]
+
+vladimir = Lecturer('Владимир', 'Афонин')
+vladimir.courses_attached += ['Git — система контроля версий']
+vladimir.grades['Git — система контроля версий'] = [10, 9, 9, 8]
+kirill.rate_lecturer(vladimir, 'Git — система контроля версий', 6)
+
+aleksander = Lecturer('Александр', 'Шкода')
+aleksander.courses_attached += ['ООП и работа с API']
+aleksander.grades['ООП и работа с API'] = [9, 10, 9, 10]
+oleg.rate_lecturer(aleksander, 'ООП и работа с API', 9)
+
+vasiliy = Reviewer('Василий', 'Жулидин')
+vasiliy.courses_attached += ['ООП и работа с API']
+vasiliy.rate_hw(oleg, 'ООП и работа с API', 4)
+
+vaycheslav = Reviewer('Вячеслав', 'Иовенко')
+vaycheslav.courses_attached += ['Git — система контроля версий']
+vaycheslav.rate_hw(kirill, 'Git — система контроля версий', 5)
+
+print('__Студенты__', '\n', sep='')
+print(kirill, '\n', sep='')
+print(oleg, '\n', sep='')
+print(f'{oleg > kirill} {oleg == kirill} {oleg != kirill}', '\n', sep='')
+print('-' * 30, '\n', sep='')
+
+print('__Лекторы__', '\n', sep='')
+print(vladimir, '\n', sep='')
+print(aleksander, '\n', sep='')
+print(f'{aleksander > vladimir} {aleksander == vladimir} {aleksander != vladimir}', '\n', sep='')
+print('-' * 30, '\n', sep='')
+
+print('__Проверяющие__', '\n', sep='')
+print(vaycheslav, '\n', sep='')
+print(vasiliy, '\n', sep='')
+print('-' * 30, '\n', sep='')
+
+studetn_lust = []
+studetn_lust += [kirill, oleg]
+
+lecturer_list = []
+lecturer_list += [vladimir, aleksander]
+
+def average_score_all(input_list):
+    average_score = 0
+    for student in input_list:
+        average_score += student.avarage_grades()
+    return average_score
+
+print(f'''Средний бал у лекторов: {average_score_all(lecturer_list):.1f}
+Средний бал у студентов: {average_score_all(studetn_lust):.1f}''')
