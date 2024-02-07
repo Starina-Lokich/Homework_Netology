92c916ebecbfe20ca38b5063039fd99e79badb35
Vadim <starinalokic@gmail.com>
2024-02-07 15:50:01 +0300

    Finished task number 2

--- working_with_file/main.py
+++ working_with_file/main.py
@@ -20,3 +20,22 @@ with open('working_with_file\list_of_dishes.txt', encoding= 'utf-8') as file:
                  'quantity': line.split(' | ')[1], 
                  'measure': line.split(' | ')[2][:-1]}
             )
+
+def get_list_shop_by_dishes(dishes, person_count):
+    dict_shop_by_dishes = {}
+    for dish in dishes:
+        for key_cook_book in cook_book.keys():
+            if dish == key_cook_book:
+                for ingredient in cook_book[dish]:
+                    if ingredient['ingredient_name'] in dict_shop_by_dishes.keys():
+                        dict_shop_by_dishes[ingredient['ingredient_name']]['quantity'] = \
+                            dict_shop_by_dishes[ingredient['ingredient_name']]['quantity'] + int(ingredient['quantity']) * person_count
+                    else:
+                        dict_shop_by_dishes[ingredient['ingredient_name']] = {
+                            'measure': ingredient['measure'], 'quantity': int(ingredient['quantity']) * person_count
+                            }
+                        # print(dict_shop_by_dishes)
+    return dict_shop_by_dishes
+
+dishesss = ['Оладьи', 'Глазунья']
+pprint(get_list_shop_by_dishes(dishesss,2))
