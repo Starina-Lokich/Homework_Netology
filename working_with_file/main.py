ea72d151b366a94b97ed76200082b45c762bfb34
Vadim <starinalokic@gmail.com>
2024-02-07 12:35:20 +0300

    Finished task number 1

---
+++ working_with_file/main.py
@@ -0,0 +1,25 @@
+from pprint import pprint
+
+with open('working_with_file\list_of_dishes.txt', encoding= 'utf-8') as file:
+    cook_book = {}
+    couter = 0
+    name_key = ''
+    for line in file:
+        couter += 1
+        if line == '\n':
+            couter = 0
+            continue
+        elif couter == 1:
+            name_key = line[0:-1]
+            cook_book[name_key] = []
+        elif couter == 2:
+            continue
+        else:
+            cook_book[name_key].append(
+                {'ingredient_name': line.split()[0],
+                 'quantity': line.split()[2], 
+                 'measure': line.split()[4]}
+            )
+
+        print(line.strip(), couter)
+pprint(cook_book)
