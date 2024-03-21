d6feffa176f64fc3f2fe2441b4502b90986f111e
Vadim <starinalokic@gmail.com>
2024-03-21 16:29:49 +0300

    Add homework

---
+++ Работа_с_SQL._Создание_БД/music_database.sql
@@ -0,0 +1,46 @@
+CREATE TABLE IF NOT EXISTS Жанр (
+	Id SERIAL PRIMARY KEY,
+	Название varchar(40)
+);
+
+CREATE TABLE IF NOT EXISTS Исполнитель (
+	Id SERIAL PRIMARY KEY,
+	Псевдоним VARCHAR(60)
+);
+
+CREATE TABLE IF NOT EXISTS Жанр_Исполнитель (
+	Жанр_id INTEGER REFERENCES Жанр(Id),
+	Исполнитель_id INTEGER REFERENCES Исполнитель(Id),
+	CONSTRAINT Ж_И PRIMARY KEY (Жанр_id, Исполнитель_id)
+);
+
+CREATE TABLE IF NOT EXISTS Альбом (
+	Id SERIAL PRIMARY KEY,
+	Название VARCHAR(60) NOT NULL,
+	Год_выпуска date NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Исполнитель_Альбом (
+	Альбом_id INTEGER REFERENCES Альбом(Id),
+	Исполнитель_id INTEGER REFERENCES Исполнитель(Id),
+	CONSTRAINT А_И PRIMARY KEY (Альбом_id, Исполнитель_id)
+);
+
+CREATE TABLE IF NOT EXISTS Трек (
+	Id SERIAL PRIMARY KEY,
+	Название VARCHAR(40),
+	Длительность VARCHAR(10),
+	Альбом_id INTEGER NOT NULL REFERENCES Альбом(id)
+);
+
+CREATE TABLE IF NOT EXISTS Сборник (
+	Id SERIAL PRIMARY KEY,
+	Название VARCHAR(60) NOT NULL,
+	Год_выпуска DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Состав_сборника (
+	Трек_id INTEGER REFERENCES Трек(Id),
+	Сборник_id INTEGER REFERENCES Сборник(Id),
+	CONSTRAINT Т_С PRIMARY KEY (Трек_id, Сборник_id)
+);
