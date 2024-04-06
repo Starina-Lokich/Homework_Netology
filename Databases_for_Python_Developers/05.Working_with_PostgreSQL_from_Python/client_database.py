7696ce7b5e3c6c297a2ad20dcb885e5e3c71defd
Vadim <starinalokic@gmail.com>
2024-04-06 18:20:42 +0300

    add new method 'creat_db_struct'

--- Databases_for_Python_Developers/05.Working_with_PostgreSQL_from_Python/client_database.py
+++ Databases_for_Python_Developers/05.Working_with_PostgreSQL_from_Python/client_database.py
@@ -11,3 +11,29 @@ def drop_db(conn):
             DROP TABLE client;
         """)
 
+
+def creat_db_struct(conn):
+    '''
+    Метод, создающий структуру БД (таблицы).
+    '''
+    with conn.cursor() as cur:
+     # таблица "Клиент" (id, фамилия, имя, почта)
+        cur.execute("""
+                    CREATE TABLE IF NOT EXISTS client(
+                    id SERIAL PRIMARY KEY,
+                    first_name VARCHAR(40) NOT NULL,
+                    last_name VARCHAR(40) NOT NULL,
+                    email VARCHAR(40) NOT NULL
+                    );
+                    """)
+        # таблица "Телефоны" (id, номер телефона, связь с клиентом по id)
+        cur.execute("""
+                    CREATE TABLE IF NOT EXISTS phones(
+                    id SERIAL PRIMARY KEY,
+                    number VARCHAR(20) UNIQUE,
+                    client_id INTEGER NOT NULL REFERENCES client(id)
+                    )
+                    """)
+        conn.commit()
+        print('Структура БД создана')
+
