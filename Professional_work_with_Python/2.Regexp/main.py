df20875ba08128987ca94279b248c1825a1f1de1
Vadim <starinalokic@gmail.com>
2024-04-24 17:55:31 +0300

    add function correct_full_name() - Разбивает ФИО на отдельные элементы списка

--- Professional_work_with_Python/2.Regexp/main.py
+++ Professional_work_with_Python/2.Regexp/main.py
@@ -10,3 +10,26 @@ def read_csv(filename: str, encoding: str) -> list:
     with open(filename, "r", encoding=encoding) as csv_file: 
         return list(csv.reader(csv_file))
     
+
+def correct_full_name(data_list: list) -> list:
+    """
+    Разбивает ФИО на отдельные элементы списка
+
+    Пример работы:
+    ['Усольцев Олег Валентинович'] -> ['Усольцев', 'Олег', 'Валентинович']
+    ['Усольцев', 'Олег Валентинович'] -> ['Усольцев', 'Олег', 'Валентинович']
+    """
+
+    for contacts in data_list:
+        if len(contacts[0].split()) > 2:
+            contacts[1] = contacts[0].split()[1]
+            contacts[2] = contacts[0].split()[2]
+            contacts[0] = contacts[0].split()[0]
+        elif len(contacts[1].split()) > 1:
+            contacts[2] = contacts[1].split()[1]
+            contacts[1] = contacts[1].split()[0]
+        elif len(contacts[0].split()) == 2:
+            contacts[1] = contacts[0].split()[1]
+            contacts[0] = contacts[0].split()[0]
+    return data_list
+
