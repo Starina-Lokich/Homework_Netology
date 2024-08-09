03f3ec1813a4cff3f5686472cd5b31b0ef329c65
Vadim <starinalokic@gmail.com>
2024-08-09 20:19:11 +0300

    finished work 'm2m-relations'

--- django_homework/2.2-databases-2/m2m-relations/articles/admin.py
+++ django_homework/2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,39 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, ArticleTag
 
+
+class ArticleTagInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        counr_is_main = 0
+        for form in self.forms:
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            for key, value in form.cleaned_data.items():
+                if key == 'is_main' and value is True:
+                    counr_is_main += 1
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+        if counr_is_main > 1:
+            raise ValidationError('Основным может быт только один раздел')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class ArticleTagInline(admin.TabularInline):
+    formset = ArticleTagInlineFormset
+    model = ArticleTag
+    extra = 1
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ArticleTagInline]
+    list_display = ('id', 'title', 'published_at',)
+    
--- django_homework/2.2-databases-2/m2m-relations/articles/models.py
+++ django_homework/2.2-databases-2/m2m-relations/articles/models.py
@@ -11,6 +11,33 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+    
+
+class Tag(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Название')
+    article = models.ManyToManyField(Article, through='ArticleTag', related_name='tags')
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+    
+
+class ArticleTag(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE,
+                                related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE,
+                            related_name='scopes', verbose_name='Раздел')
+    is_main = models.BooleanField(default=False, verbose_name='Основной')
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статьи'
+        ordering = ['-is_main', 'tag']
