9f46206db9fe91a504a0d124f9d683d9dbc772ce
Vadim <starinalokic@gmail.com>
2024-09-11 12:39:46 +0300

    finished work 'CRUD'

--- django_homework/3.2-crud/stocks_products/logistic/admin.py
+++ django_homework/3.2-crud/stocks_products/logistic/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Product, Stock, StockProduct
 # Register your models here.
+
+class StockProductInline(admin.TabularInline):
+    model = StockProduct
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description',]
+    inlines = [StockProductInline,]
+
+
+@admin.register(Stock)
+class StocktAdmin(admin.ModelAdmin):
+    list_display = ['address',]
+    inlines = [StockProductInline,]
--- django_homework/3.2-crud/stocks_products/logistic/views.py
+++ django_homework/3.2-crud/stocks_products/logistic/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
@@ -8,9 +9,15 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['title', 'description',]
+    ordering_fields = ['id', 'title', 'description', 'quantity', 'price']
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['products__id', 'products__title', 'products__description']
+    ordering_fields = ['id', 'address', 'products']
